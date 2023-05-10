2f2cb5261d3e0e71480139b578d424017148a04e
Ben Wyrosdick <ben.wyrosdick@gmail.com>
2023-05-10 17:23:02 -0500

    print

--- lights.py
+++ lights.py
@@ -36,6 +36,9 @@ def decode_color(color):
         return [degrees_to_rgb(color)]
 
 def get_board(num_pixels=50, auto_write=False, brightness=0.5, pixel_order=neopixel.RGBW):
+    print(pixel_order)
+    print(neopixel.RGBW)
+    print(neopixel.GRBW)
     return neopixel.NeoPixel(board.D18, num_pixels, auto_write=auto_write, brightness=brightness, pixel_order=pixel_order)
 
 def hls2rgb(h,l,s):
